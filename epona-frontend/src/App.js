ad7d2b520418412e23c982f6d4864e4c090f5926
Kit Walker <Neocoinkitt@gmail.com>
2024-11-04 12:52:41 +0000

    created NavBar.js and css files, and installed router

--- epona-frontend/src/App.js
+++ epona-frontend/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Epona Runners! </h1>
-    </div>
+    <Router>
+      <div className={styles.app}>
+        <NavBar />
+        <Container>
+          <h1>Home</h1>
+          <h1>Login</h1>
+          <h1>Sign-up</h1>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
