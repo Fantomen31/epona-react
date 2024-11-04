27ee1926d86850e246db032fb27f9c3c5e277e45
Kit Walker <Neocoinkitt@gmail.com>
2024-11-04 13:22:27 +0000

    updated css for navbar.js

--- epona-frontend/src/components/NavBar.js
+++ epona-frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/logo.webp';
@@ -10,29 +10,34 @@ const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" className={styles.navbar}>
       <Container className={styles.container}>
-        <Navbar.Brand as={Link} to="/" className={styles.navbarBrand}>
+        <NavLink to="/" className={styles.navbarBrand}>
           <img 
             src={logo}
             alt="Epona logo"
             height="45"
           />
           <span className={styles.brandText}>Epona: The Runners Hub</span>
-        </Navbar.Brand>
+        </NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.navbarToggler} />
         <Navbar.Collapse id="basic-navbar-nav" className={styles.navbarCollapse}>
+
           <Nav>
-            <Nav.Link as={Link} to="/" className={styles.navLink}>
+            <NavLink to="/" className={styles.navLink}>
               <FontAwesomeIcon icon={faHome} className={styles.faIcon} />
               Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login" className={styles.navLink}>
+            </NavLink>
+
+            <NavLink  to="/login" className={styles.navLink}>
               <FontAwesomeIcon icon={faSignInAlt} className={styles.faIcon} />
               Login
-            </Nav.Link>
-            <Nav.Link as={Link} to="/signup" className={styles.navLink}>
+            </NavLink>
+
+            <NavLink to="/signup" className={styles.navLink}>
               <FontAwesomeIcon icon={faUserPlus} className={styles.faIcon} />
               Sign-up
-            </Nav.Link>
+            </NavLink>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
