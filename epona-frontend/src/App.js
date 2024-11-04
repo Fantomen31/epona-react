ae79058d3818e521f1f6a20de445c821d3356610
Kit Walker <Neocoinkitt@gmail.com>
2024-11-04 13:10:15 +0000

    created and installed routing for my landing page

--- epona-frontend/src/App.js
+++ epona-frontend/src/App.js
@@ -1,23 +1,24 @@
 import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container'
+import { Route, Routes } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <Router>
-      <div className={styles.app}>
+  <div className={styles.app}>
         <NavBar />
         <Container>
-          <h1>Home</h1>
-          <h1>Login</h1>
-          <h1>Sign-up</h1>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/login" element={<h1>Login</h1>} />
+            <Route path="/signup" element={<h1>Sign-up</h1>} />
+          </Routes>
         </Container>
-      </div>
-    </Router>
+  </div>
   );
 }
 
